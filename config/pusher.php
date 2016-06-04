942607c3b344efaa829f382a4e968fae3d3538f0
Graham Campbell <graham@alt-three.com>
2016-06-04 23:13:35 +0100

    Encrypt too

--- config/pusher.php
+++ config/pusher.php
@@ -45,6 +45,7 @@ return [
             'app_id'   => env('PUSHER_APP_ID'),
             'options'  => [
                 'cluster'   => 'eu',
+                'encrypted' => true,
             ],
             'host'     => null,
             'port'     => null,
